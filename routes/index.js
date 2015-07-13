0bf80e270d86af247f3bdbb7f5b3a485ae3b0d83
Felipe Sanchez Bernal <fsbernal2000@yahoo.es>
2015-07-13 23:41:19 +0200

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,18 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+//  GET página por defecto (home)
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
+});
+
+// GET página del autor
+router.get('/author', function(req, res, next) {
+  res.render('author', { nombre: 'Felipe Sánchez Bernal',
+  						 foto: 'images/jd.jpg',
+  						 nota: 'Curso Desarrollo de servicios en la nube con HTML5, Javascript y node.js' 
+  					   }
+			 );
 });
 
 router.get('/quizes/question', quizController.question);
