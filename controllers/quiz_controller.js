51bf2e088f21ff6e4d9092faaa0185ccec1d0d04
Felipe Sanchez Bernal <fsbernal2000@yahoo.es>
2015-07-19 23:49:12 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -65,3 +65,31 @@ exports.answer = function (req, res) {
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 
 };
+
+
+
+// GET /quizes/new
+exports.new = function (req, res) {
+
+	//Se crea un objeto Quiz
+	var quiz = models.Quiz.build ( 
+			{pregunta:"Pregunta", respuesta:"Respuesta"}
+		);
+	//Se renderiza la página con el formulario de alta
+	res.render('quizes/new', {quiz : quiz});
+
+};
+
+
+// POST /quizes/create
+exports.create = function(req, res) {
+
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	// Guarda en la bd los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]})
+		.then(function(){
+				res.redirect('/quizes');  // Redirección HTTP a lista de preguntas
+		});
+
+};
--- routes/index.js
+++ routes/index.js
@@ -18,11 +18,14 @@ router.get('/author', function(req, res, next) {
 });
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load);  // autoload :quizId
+router.param('quizId', quizController.load);  
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
+
 
 module.exports = router;
