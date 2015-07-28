e0710b84c93fa43b3d4b268383b84a5409b839d7
Felipe Sanchez Bernal <fsbernal2000@yahoo.es>
2015-07-29 00:59:01 +0200

    Autenticacion y sesion

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var quizController = require('../controllers/quiz_controller');
+var quizController    = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 //  GET página por defecto (home)
 router.get('/', function(req, res, next) {
@@ -20,6 +21,11 @@ router.get('/author', function(req, res, next) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  
+
+// Definición de rutas de sesión
+router.get('/login',	sessionController.new);
+router.post('/login',   sessionController.create);
+router.get('/logout',	sessionController.destroy);
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
